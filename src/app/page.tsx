f09c77e1f8909aa0b0eec70b94b79c2c31119aeb
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-07-30 13:57:23 +1000

    feat: add intro component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Intro from "@/components/intro"
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1>Hello world</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 gap-x-8">
+     <div className="border border-black w-full max-w-[1100px] flex">
+      <Intro />
+      <div className="flex-1"></div>
+     </div>
     </main>
   );
 }
---
+++ src/components/intro.tsx
@@ -0,0 +1,9 @@
+export default function Intro() {
+  return (
+    <div className="flex-1 flex flex-col gap-y-4">
+      <h1 className="text-6xl font-bold tracking-[0.20rem] leading-[5rem] w-[80%]">a fresh & creative react developer</h1>
+      <p className="leading-[2rem] text-zinc-600">Hi I&apos;m William Lowe. A Melbourne-based web developer with a focus on NextJS, TypeScript and Tailwind.</p>
+
+    </div>
+  )
+}
