c41e3693c81e521c3db60767a98eae367f5176cf
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-07-30 15:04:18 +1000

    feat: add social component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Intro from "@/components/intro";
 import IntroPicture from "@/components/intro-picture";
+import Socials from "@/components/socials";
 
 export default function Home() {
   return (
@@ -8,8 +9,11 @@ export default function Home() {
       className="flex min-h-screen flex-col items-center justify-between p-24 gap-x-8"
     >
       <div className="flex-1 w-full max-w-[1200px] flex items-center">
-        <Intro />
+          <Intro />
         <IntroPicture />
+      </div>
+      <div className="w-full max-w-[1200px] flex items-start">
+        <Socials />
       </div>
     </main>
   );
