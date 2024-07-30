4b22081568f872f002e4d0bbc5b3e3fcf958dda1
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-07-30 21:40:38 +1000

    prettier

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { motion } from "framer-motion";
 import Link from "next/link";
 
@@ -25,30 +25,30 @@ export default function Header() {
   const linkDot = {
     inactive: {
       opacity: 0,
-      y: 8
+      y: 8,
     },
     active: {
       opacity: 1,
-      y: 0
-    }
-  }
+      y: 0,
+    },
+  };
 
   return (
     <header className="fixed top-0 left-0 z-50 px-8 py-4 flex items-center w-full bg-zinc-50">
       <Link href="#home">
-        <div className="w-12 h-12 rounded-full bg-zinc-900"/>
+        <div className="w-12 h-12 rounded-full bg-zinc-900" />
       </Link>
       <nav className="ml-auto">
         <ul className="flex gap-x-4 uppercase">
           {navLinks.map((link) => (
-            <motion.li 
-              key={link.name} 
+            <motion.li
+              key={link.name}
               initial="inactive"
               whileHover="active"
               className="flex flex-col items-center hover:font-bold transition"
             >
               <Link href={link.href}>{link.name}</Link>
-              <motion.div 
+              <motion.div
                 variants={linkDot}
                 className="w-2 h-2 rounded-full bg-zinc-900"
               />
--- src/components/intro-picture.tsx
+++ src/components/intro-picture.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import Image from "next/image";
-import profilePicture from '../../public/me.png'
+import profilePicture from "../../public/me.png";
 
 export default function IntroPicture() {
   return (
     <div className="flex-1 flex items-center justify-center">
-      <Image 
+      <Image
         src={profilePicture}
         alt="profile picture of me"
         quality={90}
@@ -15,7 +15,6 @@ export default function IntroPicture() {
         priority
         className="w-[360px] h-[360px] bg-zinc-800 rounded-full border-4 border-zinc-800 grayscale"
       />
-      {/* <div className="text-[400px] font-extrabold text-zinc-800">WL</div> */}
     </div>
   );
 }
