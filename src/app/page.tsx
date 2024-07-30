22c7a59b0f7277a5023ab982c70c623c0bd45ffd
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-07-30 14:06:09 +1000

    feat: add intro contact link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Intro from "@/components/intro"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 gap-x-8">
-     <div className="border border-black w-full max-w-[1100px] flex">
+     <div className="flex-1 w-full max-w-[1200px] flex items-center">
       <Intro />
       <div className="flex-1"></div>
      </div>
