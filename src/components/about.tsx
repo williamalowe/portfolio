e73d264bd5c364b3964ad99a464f419521f380e9
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-07-31 17:22:04 +1000

    feat: add about component

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -4,12 +4,17 @@ export default function About() {
   return (
     <section
       id="about"
-      className="relative h-screen w-full flex bg-zinc-900 text-zinc-50"
+      className="relative h-screen w-full flex bg-zinc-900 text-zinc-50 justify-center px-24"
     >
-      <div className="max-w-[1200px] p-24 flex flex-col">
-        <p className="pl-[200px] text-4xl w-[75%] tracking-wide leading-[4rem]">I am a passionate <span className="font-bold">front-end developer</span> who loves bringing digital experiences to life.</p>
+    <div className="flex items-center justify-center max-w-[1200px]">
+      <div className="flex-1">
         <AboutImage />
       </div>
+      <div className="flex-1 flex flex-col gap-y-4">
+        <p className="text-4xl tracking-wider leading-[4rem]">I am a passionate <span className="font-bold">front-end developer</span> who loves bringing digital experiences to life.</p>
+        <p className="leading-[2rem]">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit non numquam quasi fugiat quisquam iusto ipsa recusandae quia repellendus itaque dolor perferendis vitae nihil officia, dolorum hic beatae ducimus facilis.</p>
+      </div>
+    </div>
     </section>
   );
 }
