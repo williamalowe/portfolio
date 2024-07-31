9162cd18fc4d0a4301c304cd2cbe7a44a6c0a521
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-07-31 15:53:22 +1000

    feat: add about component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import About from "@/components/about";
 import Intro from "@/components/intro";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <Intro />
+      <About />
     </main>
   );
 }
---
+++ src/components/about.tsx
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <section
+      id="about"
+      className="relative h-screen w-full flex flex-col items-center justify-center bg-zinc-900 text-zinc-50"
+    >
+      <div className="flex items-center justify-center max-w-[1200px] px-24">
+        Hello world
+      </div>
+    </section>
+  );
+}
