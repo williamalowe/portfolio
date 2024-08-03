b15f9274e12dc4015ca686ff85caf30dac286c1f
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-08-03 14:49:07 +1000

    styles: add experiences component base-styles

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -12,6 +12,10 @@ export default function Experience() {
       <Link className="absolute bottom-2 left-[50%]" href="#contact">
         <BsCaretDownFill />
       </Link>
+      <h3 className="absolute -left-24 bottom-28 text-4xl text-zinc-950/40 font-bold tracking-wide uppercase -rotate-90">
+        Experiences
+      </h3>
+      <div className="absolute left-[calc(50%-120px)] top-[calc(50%-120px)] aspect-square w-[240px] bg-white rounded-full" />
     </section>
   );
 }
