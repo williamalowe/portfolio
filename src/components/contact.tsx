6a7a12aaaa3da2436a6ea6e620c8e4c4b64c4dc0
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-08-05 15:22:32 +1000

    styles: update contact component layout styles

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -7,22 +7,24 @@ export default function Contact() {
   return (
     <section
       id="contact"
-      className="relative h-screen w-full flex bg-zinc-900 text-zinc-50 justify-center"
+      className="relative h-screen w-full flex flex-col justify-center gap-y-12"
     >
-      <div className="h-[240px] flex flex-col justify-center gap-y-4 px-24  pt-24 bg-zinc-900 text-zinc-50">
+    <div className="flex flex-col mt-24 pb-12">
+      <div className="h-[240px] flex flex-col justify-center gap-y-4 px-24 bg-zinc-900 text-zinc-50">
         <div className="flex gap-x-8 items-center">
           <h3 className="text-4xl font-bold tracking-wider">
-            Want to get in touch?
+            Let&apos;s Connect.
           </h3>
           <div className="w-[60px] h-[2px] bg-zinc-600 rounded-full" />
         </div>
         <h5 className="text-zinc-600 tracking-wide text-sm">
-          Let&apos;s work together to build some cool stuff!
+          Feel free to reach out regarding employment, projects, suggestions, or even just to say hi!
         </h5>
       </div>
-      <div className="px-24">
-        <MessageForm />
-      </div>
+    </div>
+    <div className="flex-1 px-24 mt-16">
+      <MessageForm />
+    </div>
       <Link className="absolute bottom-2 left-[50%]" href="#home">
         <BsCaretUpFill />
       </Link>
--- src/components/message-form.tsx
+++ src/components/message-form.tsx
@@ -1,8 +1,15 @@
 export default function MessageForm() {
   return (
-    <form action="" className="flex-1 rounded-lg bg-zinc-900 text-zinc-50 p-4">
-      <input type="text" />
-      <input type="text" />
+    <form action="" className="border-2 w-full flex flex-col gap-y-8 rounded-lg bg-zinc-50 p-4">
+      <div>
+        <h3>Let&apos;s Connect!</h3>
+      </div>
+      <div className="flex flex-col gap-y-4">
+      <input id="name" type="text" placeholder="Name" className="border-b-2 bg-zinc-50 w-full"/>
+      <input id="email" type="email" placeholder="Email" className="border-b-2 bg-zinc-50 w-full"/>
+      <input id="subject" type="text" placeholder="Subject" className="border-b-2 bg-zinc-50 w-full"/>
+      <textarea name="message" id="message" placeholder="Message" rows={6} className="border-b-2 bg-zinc-50 w-full"></textarea>
+      </div>
     </form>
   );
 }
