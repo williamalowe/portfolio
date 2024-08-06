32548cf1946d849fc06b634e3d7fbd39f3ddb5e1
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-08-06 14:31:10 +1000

    styles: add experience component mobile styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <Intro />
       <About />
       <Projects />
-      {/* <Experience /> */}
+      <Experience />
       {/* <Contact /> */}
     </main>
   );
